75072b57ebf904e0c1f0bda2ddcb7372ffffdd78
Manishrajwar <manishsinghrajwar80@gmail.com>
2024-12-22 11:39:55 +0530

    resolved

--- src/Common/Email.jsx
+++ src/Common/Email.jsx
@@ -1,60 +1,42 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { RIGHTSIDECONTENT2 } from '../Data/PackageDetail'
 import emailjs from '@emailjs/browser';
 import { ImCross } from "react-icons/im";
 import toast from 'react-hot-toast';
 
 
-function Email({setOpenform2 , form}) {
+function Email({setOpenform2}) {
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-        // Validation logic
-        const formElements = form.current.elements;
-        const errors = [];
-    
-        if (!formElements.from_name.value.trim()) {
-          errors.push('Full Name is required.');
+  const form = useRef(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true);
+   const toastId =  toast.loading("Loading...");
+
+    const formData = new FormData(form.current);
+    console.log([...formData]);
+
+    emailjs.sendForm("service_v2wateq", 'template_cj1kbsn', form.current, {
+        publicKey: 'teMT0rnZ9JGkmP7O5',
+    })
+    .then(
+        () => {
+            console.log('SUCCESS!');
+            toast.success("Successfully Sent");
+        },
+        (error) => {
+            console.log('FAILED...', error.text);
+            toast.error("Something went wrong");
         }
-    
-        if (!formElements.from_email.value.trim()) {
-          errors.push('Email is required.');
-        } else if (!/\S+@\S+\.\S+/.test(formElements.from_email.value.trim())) {
-          errors.push('Enter a valid email address.');
-        }
-    
-        if (!formElements.from_number.value.trim()) {
-          errors.push('Phone Number is required.');
-        }
-    
-        if (!formElements.from_travel.value.trim()) {
-          errors.push('Travel Date is required.');
-        }
-    
-        if (!formElements.from_duration.value.trim()) {
-          errors.push('Duration is required.');
-        }
-    
-        if (errors.length > 0) {
-          alert(errors.join('\n')); // You can customize the error display as needed.
-          return;
-        }
-    
-        emailjs.sendForm("service_v2wateq", 'template_cj1kbsn', form.current, {
-            publicKey: 'teMT0rnZ9JGkmP7O5',
-          })
-          .then(
-            () => {
-              console.log('SUCCESS!');
-              toast.success("Successfuly Send");
-            },
-            (error) => {
-              console.log('FAILED...', error.text);
-              toast.error("Something went wrong")
-            },
-          );
-      };
-    
+    );
+
+     setLoading(false);
+    toast.dismiss(toastId);
+};
+
 
 
   return (
@@ -132,7 +114,7 @@ function Email({setOpenform2 , form}) {
   
                 ></textarea>
   
-                <button className="requeeqebtn">
+                <button disabled={loading} className="requeeqebtn">
                   <span>REQUEST ENQUIRY</span>
                 </button>
               </form>
